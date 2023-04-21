c193e1be6cb462e2f0ae0bf58b9bb136fdeef8ab
Влад Кучинський <vchelovek0@gmail.com>
2023-04-21 19:45:03 +0300

    added account api controller

---
+++ UrlShorterServiceWebApi/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UrlShorterServiceWebApi.Interfaces;
+using UrlShorterServiceWebApi.Models;
+
+namespace UrlShorterServiceWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult> Register([FromBody] RegisterUserModel user)
+        {
+            try
+            {
+                await accountService.Register(user);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+            return Accepted();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult> Login([FromBody] LoginUserModel user)
+        {
+            try
+            {
+                var token = await accountService.Login(user);
+                return Ok(token);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
