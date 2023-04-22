261e2c0f9ece94625643b985065b004659d13b57
Влад Кучинський <vchelovek0@gmail.com>
2023-04-22 18:23:33 +0300

    modified UrlModel

--- UrlShorterServiceWebApi/Models/UrlModel.cs
+++ UrlShorterServiceWebApi/Models/UrlModel.cs
@@ -1,8 +1,12 @@
-﻿namespace UrlShorterServiceWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShorterServiceWebApi.Models
 {
     public class UrlModel
     {
+        [Required]
         public string OriginalUrl { get; set; }
+        [Required]
         public string CustomUrl { get; set; }
     }
 }
