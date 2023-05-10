331deae015e2e7d2d5fa07dea1a32af67eb03011
Влад Кучинський <vchelovek0@gmail.com>
2023-05-10 18:54:57 +0300

    added login, register, logout to front

--- UrlShorterServiceWebApi/wwwroot/js/urlapp.js
+++ UrlShorterServiceWebApi/wwwroot/js/urlapp.js
@@ -7,12 +7,19 @@ function startlogin() {
     document.getElementById('login-model').style.display = 'block';
     document.getElementById('url-model').style.display = 'none';
 }
+function startRegister() {
+    document.getElementById('register-model').style.display = 'block';
+    document.getElementById('url-model').style.display = 'none';
+}
 
 document.getElementById('login-button').addEventListener('click', () => startlogin())
 
-async function createUrl() {
+document.getElementById('register-button').addEventListener('click', () => startRegister())
+
+function createUrl() {
     const originalUrlTextbox = document.getElementById('original-url').value.trim();
     const customUrlTextbox = document.getElementById('custom-url').value.trim();
+    const token = sessionStorage.getItem(accessToken);
     if (originalUrlTextbox != '') {
         if (customUrlTextbox == '') {
             const urlDto = {
@@ -28,30 +35,32 @@ async function createUrl() {
             })
             .then(response => response.json())
             .then(url => displayUrl(url))
-                .catch(error => console.error("Unable to create", error));
-        } else {
-            const token = sessionStorage.getItem(accessToken);
-            const urlDto = {
-                url: originalUrlTextbox,
-                customUrl: customUrlTextbox
-            };
-            fetch(`${urlUri}\custom`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Authorization': 'Bearer ' + token
-                },
-                body: JSON.stringify(urlDto)
-            })
-            .then(response => response.json())
-            .then(url => displayUrl(url))
             .catch(error => console.error("Unable to create", error));
+        } else {
+            if (token !== null) {
+                const urlDto = {
+                    originalUrl: originalUrlTextbox,
+                    shortUrl: customUrlTextbox
+                };
+                fetch(`${urlUri}/custom`, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + token
+                    },
+                    body: JSON.stringify(urlDto)
+                })
+                .then(response => response.json())
+                .then(url => displayUrl(url))
+                .catch(error => console.error("Unable to create", error));
+            }
         }
     }
 }
 
 
-async function login() {
+function login() {
     const emailTextbox = document.getElementById('login-email').value.trim();
     const passwordTextbox = document.getElementById('login-password').value.trim();
     const loginUser = {
@@ -67,31 +76,12 @@ async function login() {
         body: JSON.stringify(loginUser)
     })
     .then(response => response.json())
-    .then(response => response.json)
     .then(data => loginRes(data))
     .catch(error => console.error("Unable to login", error));
 }
 
-function loginRes(data) {
-    sessionStorage.setItem(accessToken, data.token);
-    document.getElementById('login-button').style.display = 'none';
-    document.getElementById('register-button').style.display = 'none';
-    document.getElementById('login-model').style.display = 'none';
-    document.getElementById('url-model').style.display = 'block';
-    document.getElementById('user-button').style.display = 'block';
-    document.getElementById('logout-button').style.display = 'block';
-}
-
-async function logout() {
-    sessionStorage.removeItem(accessToken);
-    document.getElementById('login-button').style.display = 'block';
-    document.getElementById('register-button').style.display = 'block';
-    document.getElementById('user-button').style.display = 'none';
-    document.getElementById('logout-button').style.display = 'none';
-}
-
-async function register() {
-    const emailTextbox = document.getElementByIdll('register-email').value.trim();
+function register() {
+    const emailTextbox = document.getElementById('register-email').value.trim();
     const passwordTextbox = document.getElementById('register-password').value.trim();
     const confirmPasswordTextbox = document.getElementById('register-confirm-password').value.trim();
     const registerUser = {
@@ -107,17 +97,36 @@ async function register() {
         },
         body: JSON.stringify(registerUser)
     })
-    .then(response => response.json())
-    .then(async (response) => {
-        if (response.ok === true) {
-        } else {
-                
-        }
-    })
-    .catch(error => console.error("Unable to register", error));
+    .catch(error => console.error("Unable to login", error));
 
+    document.getElementById('register-model').style.display = 'none';
+    document.getElementById('url-model').style.display = 'block';
+    
 }
+
+function loginRes(data) {
+    sessionStorage.setItem(accessToken, data.token);
+    document.getElementById('login-button').style.display = 'none';
+    document.getElementById('register-button').style.display = 'none';
+    document.getElementById('login-model').style.display = 'none';
+    document.getElementById('url-model').style.display = 'block';
+    document.getElementById('custom-url').style.display = 'block';
+    document.getElementById('user-button').style.display = 'block';
+    document.getElementById('logout-button').style.display = 'block';
+}
+
+async function logout() {
+    sessionStorage.removeItem(accessToken);
+    document.getElementById('login-button').style.display = 'block';
+    document.getElementById('register-button').style.display = 'block';
+    document.getElementById('user-button').style.display = 'none';
+    document.getElementById('custom-url').style.display = 'none';
+    document.getElementById('logout-button').style.display = 'none';
+}
+
+document.getElementById('logout-button').addEventListener('click', () => logout())
 
 function displayUrl(urlDto) {
     document.getElementById('original-url').value = urlDto.url;
+    document.getElementById('custom-url').value = '';
 }
